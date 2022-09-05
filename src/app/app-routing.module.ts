7f9e47d6bcc27faba7c54b15668aedfe23e84438
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-09-05 12:27:19 -0500

    Implementation of routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+
+
+
 import {EstadisticasComponent} from './body/estadisticas/estadisticas.component';
+import {TableroComponent} from './body/tablero/tablero.component';
 
 const routes: Routes = [
-	{path:'estadisticas',component:EstadisticasComponent}
+	{path:'estadisticas',component:EstadisticasComponent},
+	{path:'tablero',component:TableroComponent},
 	
 	
 	];
