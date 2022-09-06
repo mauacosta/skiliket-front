55b8ef4d93dc7e8a45237f9e15eb6d5d8ca16a90
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-09-06 16:58:37 -0500

    Profile module implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,32 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 
-import {EstadisticasComponent} from './body/estadisticas/estadisticas.component';
-import {TableroComponent} from './body/tablero/tablero.component';
-import {NoticiasComponent} from './body/noticias/noticias.component';
-import {MainComponent} from './body/main/main.component';
+import { EstadisticasComponent } from './body/estadisticas/estadisticas.component';
+import { TableroComponent } from './body/tablero/tablero.component';
+import { NoticiasComponent } from './body/noticias/noticias.component';
+import { MainComponent } from './body/main/main.component';
+import { PerfilComponent } from './body/perfil/perfil.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
-	{path:'estadisticas',component:EstadisticasComponent},
-	{path:'tablero',component:TableroComponent},
-	{path:'noticias',component:NoticiasComponent},
-  {path:'main', component:MainComponent}
-	
-	
-	];
+	{ path: 'estadisticas', component: EstadisticasComponent },
+	{ path: 'tablero', component: TableroComponent },
+	{ path: 'noticias', component: NoticiasComponent },
+	{ path: 'main', component: MainComponent },
+	{ path: 'sidebar', component: SidebarComponent },
+	{ path: 'header', component: HeaderComponent },
+	{ path: 'perfil', component: PerfilComponent }
+
+
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-	RouterModule.forRoot(routes),
-  ],
-  exports: [RouterModule]
+	declarations: [],
+	imports: [
+		CommonModule,
+		RouterModule.forRoot(routes),
+	],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
