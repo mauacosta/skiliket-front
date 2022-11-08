8d7e273047e6ae55edb4bfd22eb50e7e632bc5e6
Demian Cruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-11-08 17:05:52 -0600

    Commit integration 2

--- src/app/body/perfil/perfil.component.ts
+++ src/app/body/perfil/perfil.component.ts
@@ -1,34 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+	selector: 'app-perfil',
+	templateUrl: './perfil.component.html',
+	styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-  editComponents = false
+	editComponents = false
 
-  user: User = JSON.parse(localStorage.getItem('user')!);
+	user: User = JSON.parse(localStorage.getItem('user')!);
 	userData: any;
 
-  constructor(
-    public authService: AuthService
-  ) { }
+	constructor(
+		public authService: AuthService, private service: SharedService
+	) { }
+	NoticiaList: any = [];
+	QuejaList: any = [];
+	contadorNoticias: number = 0;
+	contadorQuejas: number = 0;
+	contadorTotal: number = 0;
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 
-    this.userData = this.authService.GetUserData(this.user).subscribe((data) => {
+		this.userData = this.authService.GetUserData(this.user).subscribe((data) => {
 			this.userData = data.data();
+			this.refreshNoticiaList();
+			this.refreshQuejaList();
 		})
 
-  }
-  editComponentsProfile(): void{
-    this.editComponents=!this.editComponents
-  }
-  editProfile():void{
-    console.log("editando")
-  }
+	}
+	refreshNoticiaList() {
+		this.service.getNoticiasList().subscribe(data => {
+			this.NoticiaList = data;
+			console.log(this.NoticiaList);
+			for (let i = 0; i < this.NoticiaList.length; i++) {
+				this.contadorNoticias++;
+				console.log("Numero de eventos: " +this.contadorNoticias);
+		}
+		});
+	}
+	refreshQuejaList() {
+		this.service.getQuejasList().subscribe(data => {
+			this.QuejaList = data;
+			console.log(this.QuejaList);
+			for (let j = 0; j < this.QuejaList.length; j++) {
+				this.contadorQuejas++;
+				console.log("Numero de eventos: " +this.contadorQuejas);
+		}
+		this.contadorTotal = this.contadorNoticias + this.contadorQuejas;
+		});
+	}
+
+
+editComponentsProfile(): void {
+	this.editComponents = !this.editComponents
+}
+editProfile(): void {
+	console.log("editando")
+}
 
 }
+
