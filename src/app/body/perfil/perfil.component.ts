dbdf32da78dd88864631b1574a3c114124c3c9fd
Demian Cruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-11-08 09:06:46 -0600

    Insigneas asignadas

--- src/app/body/perfil/perfil.component.ts
+++ src/app/body/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+  NoticiaList: any = [];
+  QuejaList: any = [];
+  contadorNoticias: number = 0;
+  contadorQuejas: number = 0;
+  contadorTotal: number = 0;
 
-  ngOnInit(): void {
-  }
+  
+	ngOnInit(): void {
+		this.refreshNoticiaList();
+		this.refreshQuejaList();
+	}
+	refreshNoticiaList() {
+		this.service.getNoticiasList().subscribe(data => {
+			this.NoticiaList = data;
+			console.log(this.NoticiaList);
+			for (let i = 0; i < this.NoticiaList.length; i++) {
+				this.contadorNoticias++;
+				console.log("Numero de eventos: " +this.contadorNoticias);
+		}
+		});
+	}
+	refreshQuejaList() {
+		this.service.getQuejasList().subscribe(data => {
+			this.QuejaList = data;
+			console.log(this.QuejaList);
+			for (let j = 0; j < this.QuejaList.length; j++) {
+				this.contadorQuejas++;
+				console.log("Numero de eventos: " +this.contadorQuejas);
+		}
+		this.contadorTotal = this.contadorNoticias + this.contadorQuejas;
+		});
+	}
 
 }
