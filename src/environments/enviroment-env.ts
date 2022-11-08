53194b25ad0b42b2fdea9f99a040d56974bd7477
Demian Cruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-11-08 09:47:44 -0600

    Comit env file

---
+++ src/environments/enviroment-env.ts
@@ -0,0 +1,17 @@
+export const env = {
+    weather: {
+        climaApiUrl: 'https://open-weather13.p.rapidapi.com/city/mexico%2520city',
+        XRapidAPIHostLabel: 'X-RapidAPI-Host',
+        XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+        XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+        XRapidAPIKeyHeaderValue: '117e5349c3mshf6db570f4919973p1c8aedjsnba7adc1dcc02',
+    },
+    firebase: {
+        apiKey: 'AIzaSyAp5UsGRRhN0m77-w6X2-UXrZCjwCf4wMs',
+        authDomain: 'smooth-league-364220.firebaseapp.com',
+        projectId: 'smooth-league-364220',
+        storageBucket: 'smooth-league-364220.appspot.com',
+        messagingSenderId: '214332462127',
+        appId: '1:214332462127:web:007416c81e1534289f62da',
+    },
+};
