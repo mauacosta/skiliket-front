779e7963e6a65ba5ca9e936955f202e2e544feeb
Demian Cruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-11-08 07:18:27 -0600

    Pareto funtionality per month pt.1/12

--- src/app/body/estadisticas/estadisticas.component.ts
+++ src/app/body/estadisticas/estadisticas.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { ActivatedRoute } from '@angular/router';
 import { ThisReceiver } from '@angular/compiler';
-import {Mes} from './mes';
+import { Mes } from './mes';
 import { SharedService } from 'src/app/shared.service';
 
 
@@ -29,7 +29,7 @@ accessibility(Highcharts);
 export class EstadisticasComponent implements OnInit {
 	mes: Mes[] | undefined;
 
-	constructor(public _Activatedroute: ActivatedRoute,  private service: SharedService) { }
+	constructor(public _Activatedroute: ActivatedRoute, private service: SharedService) { }
 	QuejaList: any = [];
 	contadorServiciosPublicos: number = 0;
 	contadorRobos: number = 0;
@@ -38,6 +38,8 @@ export class EstadisticasComponent implements OnInit {
 	contadorContaminacion: number = 0;
 	contadorMuchoRuido: number = 0;
 	contadorOtros: number = 0;
+	fechaArreglo: any = [];
+	mesEstadistica: string = "";
 
 	public options: any = {
 		chart: {
@@ -124,86 +126,92 @@ export class EstadisticasComponent implements OnInit {
 			this.QuejaList = data;
 			console.log(this.QuejaList);
 			//for in QuejasList
-			for (let i = 0; i < this.QuejaList.length; i++) {
-				//if naturaleza == 'Servicios públicos (Gas, electricidad, agua)'
-				if (this.QuejaList[i].naturaleza === 'Servicios públicos (Gas, electricidad, agua)') {
-					this.contadorServiciosPublicos++;
-				} else
-					//if naturaleza == 'Robos'
-					if (this.QuejaList[i].naturaleza === 'Robos') {
-						this.contadorRobos++;
-					} else
-						//if naturaleza == 'Inundaciones'
-						if (this.QuejaList[i].naturaleza === 'Inundaciones') {
-							this.contadorInundaciones++;
+			if (this._Activatedroute.snapshot.paramMap.get('Mes') == "Octubre") {
+				for (let i = 0; i < this.QuejaList.length; i++) {
+					this.fechaArreglo = this.QuejaList[i].fecha.split("-");
+					this.mesEstadistica = this.fechaArreglo[1].split(',');
+					if (this.mesEstadistica[0] == "10") {
+						//if naturaleza == 'Servicios públicos (Gas, electricidad, agua)'
+						if (this.QuejaList[i].naturaleza === 'Servicios públicos (Gas, electricidad, agua)') {
+							this.contadorServiciosPublicos++;
 						} else
-							//if naturaleza == 'Incendios'
-							if (this.QuejaList[i].naturaleza === 'Incendios') {
-								this.contadorIncendios++;
+							//if naturaleza == 'Robos'
+							if (this.QuejaList[i].naturaleza === 'Robos') {
+								this.contadorRobos++;
 							} else
-							//if naturaleza == 'Contaminación'
-							if (this.QuejaList[i].naturaleza === 'Contaminación') {
-								this.contadorContaminacion++;
-							} else
-								//if naturaleza == 'Mucho ruido'
-								if (this.QuejaList[i].naturaleza === 'Mucho ruido') {
-									this.contadorMuchoRuido++;
+								//if naturaleza == 'Inundaciones'
+								if (this.QuejaList[i].naturaleza === 'Inundaciones') {
+									this.contadorInundaciones++;
 								} else
-									//if naturaleza == 'Otros'
-									if (this.QuejaList[i].naturaleza === 'Otros') {
-										this.contadorOtros++;
-									}
-			}
-			if (this.contadorServiciosPublicos > 0 || this.contadorContaminacion > 0 ||
-				this.contadorInundaciones > 0 || this.contadorMuchoRuido > 0 ||
-				this.contadorOtros > 0 || this.contadorRobos > 0 || this.contadorIncendios > 0) {
-				this.options.series[1]['data'].push(this.contadorServiciosPublicos);
-				this.options.series[1]['data'].push(this.contadorRobos);
-				this.options.series[1]['data'].push(this.contadorInundaciones);
-				this.options.series[1]['data'].push(this.contadorIncendios);
-				this.options.series[1]['data'].push(this.contadorContaminacion);
-				this.options.series[1]['data'].push(this.contadorMuchoRuido);
-				this.options.series[1]['data'].push(this.contadorOtros);
-				console.log("Contaminacion: " + this.contadorContaminacion);
-				console.log("Inundaciones: " + this.contadorInundaciones);
-				console.log("Incendios: " + this.contadorIncendios);
-				console.log("Mucho ruido: " + this.contadorMuchoRuido);
-				console.log("Otros: " + this.contadorOtros);
-				console.log("Robos: " + this.contadorRobos);
-				console.log("Servicios publicos: " + this.contadorServiciosPublicos);
-
-			}
-				else {
-					this.options.series[0]['data'][0] = {
-						name: 'No existen quejas sobre ningun tema',
-						y: 10
-					};
+									//if naturaleza == 'Incendios'
+									if (this.QuejaList[i].naturaleza === 'Incendios') {
+										this.contadorIncendios++;
+									} else
+										//if naturaleza == 'Contaminación'
+										if (this.QuejaList[i].naturaleza === 'Contaminación') {
+											this.contadorContaminacion++;
+										} else
+											//if naturaleza == 'Mucho ruido'
+											if (this.QuejaList[i].naturaleza === 'Mucho ruido') {
+												this.contadorMuchoRuido++;
+											} else
+												//if naturaleza == 'Otros'
+												if (this.QuejaList[i].naturaleza === 'Otros') {
+													this.contadorOtros++;
+												}
+					}
 				}
-				Highcharts.chart('container', this.options);
-		});
-	}
-		
+					if (this.contadorServiciosPublicos > 0 || this.contadorContaminacion > 0 ||
+						this.contadorInundaciones > 0 || this.contadorMuchoRuido > 0 ||
+						this.contadorOtros > 0 || this.contadorRobos > 0 || this.contadorIncendios > 0) {
+						this.options.series[1]['data'].push(this.contadorServiciosPublicos);
+						this.options.series[1]['data'].push(this.contadorRobos);
+						this.options.series[1]['data'].push(this.contadorInundaciones);
+						this.options.series[1]['data'].push(this.contadorIncendios);
+						this.options.series[1]['data'].push(this.contadorContaminacion);
+						this.options.series[1]['data'].push(this.contadorMuchoRuido);
+						this.options.series[1]['data'].push(this.contadorOtros);
+						console.log("Contaminacion: " + this.contadorContaminacion);
+						console.log("Inundaciones: " + this.contadorInundaciones);
+						console.log("Incendios: " + this.contadorIncendios);
+						console.log("Mucho ruido: " + this.contadorMuchoRuido);
+						console.log("Otros: " + this.contadorOtros);
+						console.log("Robos: " + this.contadorRobos);
+						console.log("Servicios publicos: " + this.contadorServiciosPublicos);
 
-/*
-	poblarQuejas(){
-		this.QuejasList = [
-			{ Queja: "Servicios públicos (Gas, electricidad, agua)", Cantidad: 755 },
-			{ Queja: "Robos", Cantidad: 222 },
-			{ Queja: "Inundaciones", Cantidad: 151 },
-			{ Queja: "Incendios", Cantidad: 123 },
-			{ Queja: "Tráfico", Cantidad: 86 },
-			{ Queja: "Contaminación", Cantidad: 72 },
-			{ Queja: "Suciedad", Cantidad: 51 },
-			{ Queja: "Mucho ruido", Cantidad: 36 },
-			{ Queja: "Otros", Cantidad: 10 }
-		];
-		//console log QuejasList
-		console.log(this.QuejasList);
-		//Poblar el gráfico con los datos de la lista
-		for (let i = 0; i < this.QuejasList.length; i++) {
-			this.options.series[1].data.push(this.QuejasList[i].Cantidad);
-		}
+					}
+					else {
+						this.options.series[0]['data'][0] = {
+							name: 'No existen quejas sobre ningun tema',
+							y: 10
+						};
+					}
+					Highcharts.chart('container', this.options);
+				}
+			});
 	}
-	*/
+
+
+	/*
+		poblarQuejas(){
+			this.QuejasList = [
+				{ Queja: "Servicios públicos (Gas, electricidad, agua)", Cantidad: 755 },
+				{ Queja: "Robos", Cantidad: 222 },
+				{ Queja: "Inundaciones", Cantidad: 151 },
+				{ Queja: "Incendios", Cantidad: 123 },
+				{ Queja: "Tráfico", Cantidad: 86 },
+				{ Queja: "Contaminación", Cantidad: 72 },
+				{ Queja: "Suciedad", Cantidad: 51 },
+				{ Queja: "Mucho ruido", Cantidad: 36 },
+				{ Queja: "Otros", Cantidad: 10 }
+			];
+			//console log QuejasList
+			console.log(this.QuejasList);
+			//Poblar el gráfico con los datos de la lista
+			for (let i = 0; i < this.QuejasList.length; i++) {
+				this.options.series[1].data.push(this.QuejasList[i].Cantidad);
+			}
+		}
+		*/
 
 }
